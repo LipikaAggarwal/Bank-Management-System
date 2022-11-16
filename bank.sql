7bb98a1d8894f04b3baf3237ec8efc48edd5a429
LipikaAggarwal <86905882+LipikaAggarwal@users.noreply.github.com>
2022-11-17 05:05:22 +0530

    Update bank.sql

--- bank.sql
+++ bank.sql
@@ -14,6 +14,6 @@ PASSWORD varchar(12));
 
 select * from users;
 select * from admins;
--- select NAME from users where NAME="%s";
+
 insert into admins(USERNAME,NAME,PASS) values('DS','imdr',2022);
 insert into users(ACCNO,NAME,PHNO,AMOUNT,PIN) values(10000,'mahesh',999999999,1000,14);
